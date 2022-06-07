81f008c1d4f0012fe390a59e11c56be78ce9d551
Core33DC <ning0790@gmail.com>
2022-06-07 17:40:53 +0900

    220607 커밋

---
+++ Day05/WpfMvvmApp/ViewModels/RelayCommand.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfMvvmApp.ViewModels
+{
+    /// <summary>
+    /// ViewModel과 View를 Glue하기 위한
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> execute;  //실행처리를 위한 Generic
+        private readonly Predicate<T> canExcute;  //실행여부를 판단하는 generic
+
+        //실행여부에 따라서 이벤트를 추가해주거나 삭제하는 이벤트핸들러
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExcute?.Invoke((T)parameter) ?? true;
+            //throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            execute((T)parameter);
+            //throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// execute만 파라미터 받는 생성자
+        /// </summary>
+        /// <param name="execute"></param>
+        public RelayCommand(Action<T> execute) : this(execute, null) { }
+
+        /// <summary>
+        /// execute, canExecute를 파라미터로 받는 생성자
+        /// </summary>
+        /// <param name="execute"></param>
+        /// <param name="canExcute"></param>
+        public RelayCommand(Action<T> execute, Predicate<T> canExcute = null)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExcute = canExcute;
+        }
+    }
+}
---
+++ Day05/WpfMvvmApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace WpfMvvmApp.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+
+            //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            //위에 11,12번째 줄하고 똑같은 의미이다.
+        }
+    }
+}
